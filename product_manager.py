9cc5d05b90a7e40884dfcd301a0a072e748f5a92
iRazvanCM <mrazvan.constantin94@gmail.com>
2026-09-08 12:24:12 +0200

    Added remove_product method to ProductManager

--- product_manager.py
+++ product_manager.py
@@ -25,3 +25,19 @@ class ProductManager:
         for product in self.products:
             total += product.price * product.quantity
         return total
+
+
+
+
+    def remove_product(self, name):
+        """Elimina un produs dupa nume."""
+        for product in self.products:
+            if product.name == name:
+                self.products.remove(product)
+                print(f"Produsul '{name}' a fost eliminat din inventar.")
+                return
+        print(f"Produsul '{name}' nu a fost gasit.")    
+
+
+
+    
